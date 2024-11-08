138551e702e87f96dae3af2ae641aed525067573
Fromage <florian.marcot@hotmail.fr>
2024-11-08 16:59:27 +0100

    Classe et relations finis

--- traitement-fichier-jpa-off/src/main/java/dev/fr/entity/Allergene.java
+++ traitement-fichier-jpa-off/src/main/java/dev/fr/entity/Allergene.java
@@ -1,5 +1,26 @@
 package dev.fr.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Allergene {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nom", unique = true, nullable = false, length = 100)
+    private String nom;
+
+    public Allergene() {
+    }
+
+    public Allergene(String nom) {
+        this.nom = nom;
+    }
 
 }
